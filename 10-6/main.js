cfc1f7c0022d827488e55436ac32cd39ed1e2c79
Robert Dziubek <robert.dziubek5@gmail.com>
2020-03-23 17:13:15 +0100

    Add module 6 elements

--- 10-6/main.js
+++ 10-6/main.js
@@ -1,10 +1,39 @@
-const inputs = document.getElementsByClassName('input');
+const thumbs = document.querySelectorAll('.gallery > *');
+const fullImages = document.querySelectorAll('.output > img');
+const attributions = document.querySelectorAll('.output > span');
+const allOutputElements = document.querySelectorAll('.output *');
 
-function alterStyle() {
-    this.name = 'new_name';
-    this.style.fontWeight = 'bold';
-    this.style.color = 'white';
-    this.style.backgroundColor = 'red';
+function showCorrespondingAttrib(e) {
+    function isAnyVisible(attribs) {
+        for (const attrib of attribs) if (attrib.style.display !== '') return true;
+        return false;
+    }
+
+    if (e.type === 'mouseover') resetView(e);
+    if (!isAnyVisible(attributions)) {
+        for (const attrib of attributions) if (attrib.id.includes(e.target.id))
+            attrib.style.display = 'initial';
+    }
 }
 
-for (const input of inputs) input.addEventListener('mousedown', alterStyle);
+function showCorrespondingImageAndAttrib(e) {
+    resetView(e);
+    for (const fullImage of fullImages) if (fullImage.id.includes(e.target.id))
+        fullImage.style.display = 'initial';
+    showCorrespondingAttrib(e);
+}
+
+function resetView(e) {
+    function isAnyVisible(images) {
+        for (const image of images) if (image.style.display !== '') return true;
+        return false;
+    }
+
+    if (!isAnyVisible(fullImages) || e.type !== 'mouseout') {
+        for (const element of allOutputElements) element.style.display = '';
+    }
+}
+
+for (const thumb of thumbs) thumb.addEventListener('mouseover', showCorrespondingAttrib);
+for (const thumb of thumbs) thumb.addEventListener('mouseout', resetView);
+for (const thumb of thumbs) thumb.addEventListener('mousedown', showCorrespondingImageAndAttrib);
