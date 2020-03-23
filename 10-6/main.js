1bc2025b626a21c729751ee4805b2e34150782ee
Robert Dziubek <robert.dziubek5@gmail.com>
2020-03-23 21:59:57 +0100

    Change view event and style attribs

--- 10-6/main.js
+++ 10-6/main.js
@@ -3,37 +3,12 @@ const fullImages = document.querySelectorAll('.output > img');
 const attributions = document.querySelectorAll('.output > span');
 const allOutputElements = document.querySelectorAll('.output *');
 
-function showCorrespondingAttrib(e) {
-    function isAnyVisible(attribs) {
-        for (const attrib of attribs) if (attrib.style.display !== '') return true;
-        return false;
-    }
-
-    if (e.type === 'mouseover') resetView(e);
-    if (!isAnyVisible(attributions)) {
-        for (const attrib of attributions) if (attrib.id.includes(e.target.id))
-            attrib.style.display = 'initial';
-    }
-}
-
-function showCorrespondingImageAndAttrib(e) {
-    resetView(e);
+function updateView(e) {
+    for (const element of allOutputElements) element.style.display = '';
     for (const fullImage of fullImages) if (fullImage.id.includes(e.target.id))
         fullImage.style.display = 'initial';
-    showCorrespondingAttrib(e);
+    for (const attrib of attributions) if (attrib.id.includes(e.target.id))
+        attrib.style.display = 'initial';
 }
 
-function resetView(e) {
-    function isAnyVisible(images) {
-        for (const image of images) if (image.style.display !== '') return true;
-        return false;
-    }
-
-    if (!isAnyVisible(fullImages) || e.type !== 'mouseout') {
-        for (const element of allOutputElements) element.style.display = '';
-    }
-}
-
-for (const thumb of thumbs) thumb.addEventListener('mouseover', showCorrespondingAttrib);
-for (const thumb of thumbs) thumb.addEventListener('mouseout', resetView);
-for (const thumb of thumbs) thumb.addEventListener('mousedown', showCorrespondingImageAndAttrib);
+for (const thumb of thumbs) thumb.addEventListener('mouseover', updateView);
