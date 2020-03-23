48494246997530e6cb654790e47d966ecc40211f
Robert Dziubek <robert.dziubek5@gmail.com>
2020-03-23 16:38:30 +0100

    Fix elements stacking on top of each other

--- 10-5/main.js
+++ 10-5/main.js
@@ -4,25 +4,35 @@ const attributions = document.querySelectorAll('.output > span');
 const allOutputElements = document.querySelectorAll('.output *');
 
 function showCorrespondingAttrib(e) {
-    for (const attrib of attributions) if (attrib.id.includes(e.target.id))
-        attrib.style.display = 'initial';
+    if (e.type === 'mouseover') {
+        resetView(e);
+    }
+
+    function isAnyVisible(attribs) {
+        for (const attrib of attribs) if (attrib.style.display !== '') return true;
+        return false;
+    }
+
+    if (!isAnyVisible(attributions)) {
+        for (const attrib of attributions) if (attrib.id.includes(e.target.id))
+            attrib.style.display = 'initial';
+    }
 }
 
 function showCorrespondingImageAndAttrib(e) {
-    resetView();
+    resetView(e);
     for (const fullImage of fullImages) if (fullImage.id.includes(e.target.id))
         fullImage.style.display = 'initial';
     showCorrespondingAttrib(e);
 }
 
-function resetView() {
+function resetView(e) {
     function isAnyVisible(images) {
         for (const image of images) if (image.style.display !== '') return true;
-
         return false;
     }
 
-    if (!isAnyVisible(fullImages)) {
+    if (!isAnyVisible(fullImages) || e.type !== 'mouseout') {
         for (const element of allOutputElements) element.style.display = '';
         console.log('none are visible, clearing');
     }
