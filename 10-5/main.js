a95eedcb2cf1288543b88021918110cc172843cb
Robert Dziubek <robert.dziubek5@gmail.com>
2020-03-23 12:37:04 +0100

    Implement attribution showing
    + necessary styling (add display container, init elements)

--- 10-5/main.js
+++ 10-5/main.js
@@ -1,12 +1,29 @@
 const thumbs = document.querySelectorAll('.gallery > *');
+const fullImages = document.querySelectorAll('.output > img');
+const attributions = document.querySelectorAll('.output > span');
+const allOutputElements = document.querySelectorAll('.output *');
 
 function showCorrespondingAttrib() {
-    console.log('on ' + this.id);
+    for (const attrib of attributions) if (attrib.id.includes(this.id))
+        attrib.style.display = 'initial';
+}
+
+function showCorrespondingImageAndAttrib() {
+    showCorrespondingAttrib();
 }
 
 function resetView() {
-    console.log('reset');
+    function areNoneVisible(images) {
+        for (const image of images) if (image.style.display !== '') return false;
+
+        return true;
+    }
+
+    if (areNoneVisible(fullImages)) {
+        for (const element of allOutputElements) element.style.display = '';
+    }
 }
 
 for (const thumb of thumbs) thumb.addEventListener('mouseover', showCorrespondingAttrib);
 for (const thumb of thumbs) thumb.addEventListener('mouseout', resetView);
+for (const thumb of thumbs) thumb.addEventListener('mousedown', showCorrespondingImageAndAttrib);
